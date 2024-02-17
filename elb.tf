c5352c0d5b9819d78d4017e0f3f796ae7a26d4e1
veerperu1712 <150333089+veerperu1712@users.noreply.github.com>
2024-02-17 14:17:15 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
+  name               = "peru-terraform-elb"
   availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
 
   listener {
@@ -25,19 +25,19 @@ resource "aws_elb" "bar" {
   }
 }
 
- /*resource "aws_rds_cluster" "default" {
+  resource "aws_rds_cluster" "default" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.11.2"
   availability_zones      = ["ap-southeast-1a", "ap-southeast-1b"]
   database_name           = "mydb"
-  master_username         = "raham"
-  master_password         = "Rahamshaik#444555"
+  master_username         = "peru"
+  master_password         = "Eagleeye@1712"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
-}*/
+}
 
-/*resource "aws_instance" "one" {
+  resource "aws_instance" "one" {
   for_each = toset(["one", "two", "three"])
   ami           = "ami-006be9ab6a140de6e"
   instance_type = "t2.micro"
@@ -45,4 +45,4 @@ resource "aws_elb" "bar" {
     Name = "instance-${each.key}"
   }
 }
-*/
+
